8c88a670669e3a22a65548245211a48361fef3c8
Junonia Wren <wrenjunonia@gmail.com>
2026-02-18 14:02:50 -0600

    Cambio de escena de Menu y Juego

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import {Game} from './game.js';
+import { MenuScene } from './MenuScene.js';
+
 
 
 
@@ -7,7 +9,7 @@ const config={
     width: 800,
     height: 600,
     backgroundColor: '#a5d2e4',
-    scene:[Game],
+    scene:[Game, MenuScene],
     physics:{
         default: 'arcade',
         arcade:{
